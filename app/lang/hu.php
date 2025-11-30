3ec594ba372c902ace3cdd6a5e737a4d0d7d6114
Kovács Nándor <43203136+hydroxyweb@users.noreply.github.com>
2025-11-30 08:22:29 +0100

    Indentation

--- app/lang/hu.php
+++ app/lang/hu.php
@@ -2,18 +2,18 @@
 return [
     'cant-load-statistic' => 'Nem sikerült betölteni ehhez a hónaphoz tartozó statisztikákat',
     'months' => 
-    [
-        'január',
-        'február',
-        'március',
-        'április',
-        'május',
-        'június',
-        'július',
-        'augusztus',
-        'szeptember',
-        'október',
-        'november',
-        'december'
-    ]
+        [
+            'január',
+            'február',
+            'március',
+            'április',
+            'május',
+            'június',
+            'július',
+            'augusztus',
+            'szeptember',
+            'október',
+            'november',
+            'december'
+        ]
 ];
